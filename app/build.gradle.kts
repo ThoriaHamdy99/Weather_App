8cce5563d9c3dee5f2099c7f5c046a64cdfc5f6a
Thoraya <thoriahamdy99@gmail.com>
2025-06-10 18:53:08 +0300

    add koin dependencies, ktor dependencies and any other dependencies that will be used

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,33 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    koinDependencies()
+    coroutinesDependencies()
+    ktorDependencies()
+
+    // Koin Android features
+    implementation(libs.koin.android)
+}
+
+fun ktorDependencies() {
+    dependencies {
+        implementation(libs.ktor.client.core)
+        implementation(libs.ktor.client.cio)
+        implementation(libs.ktor.client.content.negotiation)
+        implementation(libs.ktor.serialization.kotlinx.json)
+    }
+}
+
+fun coroutinesDependencies() {
+    dependencies {
+        implementation(libs.kotlinx.coroutines.android)
+    }
+}
+
+fun koinDependencies() {
+    dependencies {
+        implementation(libs.koin.core)
+
+    }
 }
