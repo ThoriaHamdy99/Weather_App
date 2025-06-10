9b50dcbe821f415461133aff474aa2724eb74447
Thoraya <thoriahamdy99@gmail.com>
2025-06-10 19:06:39 +0300

    update weather status ui model

--- app/src/main/java/com/example/weather_app/presentation/model/CurrentWeatherStatus.kt
+++ app/src/main/java/com/example/weather_app/presentation/model/CurrentWeatherStatus.kt
@@ -7,10 +7,10 @@ enum class CurrentWeatherStatus(
     val iconResId: Int,
     val measureTool: String
 ) {
-    WIND("Wind", R.drawable.fast_wind_icon, "KM/h"),
+    WIND("Wind", R.drawable.fast_wind_icon, " KM/h"),
     HUMIDITY("Humidity", R.drawable.humidity_icon, "%"),
     RAIN("Rain", R.drawable.rain_icon, "%"),
     UV_INDEX("Uv Index", R.drawable.uv_icon, ""),
-    PRESSURE("Pressure", R.drawable.pressure_icon, "hPa"),
+    PRESSURE("Pressure", R.drawable.pressure_icon, " hPa"),
     FEELS_LIKE("Feels like", R.drawable.temperature_icon, "°C"),
 }
