d73e4d37cc64546f823f001430283919a3cbfe71
Thoraya <thoriahamdy99@gmail.com>
2025-06-14 00:10:04 +0300

    adjust colors in weather state

--- app/src/main/java/com/example/weather_app/ui/theme/Color.kt
+++ app/src/main/java/com/example/weather_app/ui/theme/Color.kt
@@ -8,3 +8,6 @@ val DarkBluishPurpleColor = Color(0xFF0D0C19)
 val LightSkyBlueColor = Color(0xFF87CEFA)
 val WhiteColor = Color(0xFFFFFFFF)
 val DarkGrayColor = Color(0xFF323232)
+
+val LightDefaultShadowColor = Color(0xFFFFC701)
+val DarkDefaultShadowColor = Color(0xFF7E2FFF)
